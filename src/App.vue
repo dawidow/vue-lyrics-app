096b35e17a5300db301c6c8fad5cdfebe4dd9630
David Owczarz <owcdaw@gmail.com>
2019-10-10 22:45:17 +0200

    added new components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template>
   <div id="app">
 	  <div class="header">Find the text for the music you are listening to</div>
-	  <div class="search">
-		<el-input placeholder="Write author or title of the song..." v-model="input" clearable></el-input>
-	  </div>
+	  <form @submit.prevent="handleSubmit" class="search">
+		<el-input @submit.prevent="handleSubmit" placeholder="Write author or title of the song..." v-model="search" clearable></el-input>
+		<div class="search-icon-cont">
+			<div class="search-icon"></div>
+		</div>
+	</form>
+	<Results :results="matchLyrics" />
     <router-view/>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Results from './components/Results';
+
 export default {
   name: 'App',
+  components: {
+	  Results
+  },
   data() {
 	  return {
-		  title: ''
+		  search: '',
+		  matchLyrics: []
+	  }
+  },
+  methods: {
+	  handleSubmit() {
+		  if(this.search && !this.search.trim().length == 0) {
+
+			  axios.get(`https://api.genius.com/search?q=${this.search}&access_token=J2TNQOOrgxMW2kpv2i6EFcrHssL2gII5veB2p23SLOdqhLeEn1812yjY0mc2p1ge`)
+				  .then(response => this.matchLyrics = response.data.response.hits)
+				.catch(err => console.log(err))
+		  }
 	  }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Poppins:300,500,700&display=swap');
 @import url('https://fonts.googleapis.com/css?family=Mansalva&display=swap');
 
@@ -30,17 +51,44 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
 
-.header {
+  .header {
 	font-family: 'Mansalva', sans-serif;
 	font-size: 30px;
 	margin-bottom: 20px;
 
 }
 
-.search {
+	.search {
+		width: 90%;
+		margin: 0 auto;
+	}
+}
+
+.results {
 	width: 90%;
-	margin: 0 auto;
+	margin: 30px auto 0;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	// align-items: center;
+	flex-direction: row;
+
+	.result {
+		width: 20%;
+		min-height: 350px;
+		flex-direction: column;
+		justify-content: space-between;
+
+		&__image {
+			border-radius: 50%;
+			height: 120px;
+			width: 120px;
+		}
+
+		&__link {
+			font-size: 12px;
+		}
+	}
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="results">
+		<div class="result" v-for="lyric in results" :key="lyric.result.id">
+				<img :src="lyric.result.header_image_url" class="result__image">
+				<h3>{{ lyric.result.primary_artist.name }}</h3>
+				<p>{{ lyric.result.title_with_featured }}</p>
+				<a :href="lyric.result.url" class="result__link"><i class="fa fa-external-link" aria-hidden="true"></i> Czytaj tekst na stronie genius.com</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'results',
+	props: ['results'],
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
