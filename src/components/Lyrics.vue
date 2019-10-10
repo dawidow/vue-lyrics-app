f66edd156d4ad4593ba993c9f74748fbcc230693
David Owczarz <owcdaw@gmail.com>
2019-10-10 23:12:26 +0200

    added responsive

--- src/components/Lyrics.vue
+++ src/components/Lyrics.vue
@@ -1,15 +1,15 @@
 <template>
-
+	<div class="container">
+		<h2 class="lyrics__author">{{ lyric.result.primary_artist.name }}</h2>
+		<p class="lyrics__title"></p>
+		<p class="lyrics__text"></p>
+	</div>
 </template>
 
 <script>
 export default {
   name: 'lyricsView',
-  data () {
-    return {
-
-    }
-  }
+  props: ['lyric']
 }
 </script>
 
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -4,8 +4,9 @@
 				<img :src="lyric.result.header_image_url" class="result__image">
 				<h3>{{ lyric.result.primary_artist.name }}</h3>
 				<p>{{ lyric.result.title_with_featured }}</p>
-
-				<a :href="lyric.result.url" class="result__link"><font-awesome-icon icon="external-link-alt" /> Czytaj tekst na stronie genius.com</a>
+				<button class="result__btn">Read the lyrics<lyricsView :lyric="results" /></button>
+				<a :href="lyric.result.url" class="result__link"><font-awesome-icon icon="external-link-alt" />
+				Read the lyrics on genius.com</a>
 		</div>
 	</div>
 </template>
@@ -23,21 +24,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.results {
-	width: 90%;
-	margin: 30px auto 0;
-	display: flex;
-	flex-wrap: wrap;
-	// justify-content: space-between;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-
-	.result {
-		width: 100%;
-		min-height: 350px;
+	.results {
+		width: 90%;
+		margin: 30px auto 0;
+		display: flex;
+		flex-wrap: wrap;
+		// justify-content: space-between;
+		align-items: center;
+		justify-content: center;
 		flex-direction: column;
-		justify-content: space-between;
+
+		.result {
+			width: 100%;
+			min-height: 350px;
+			flex-direction: column;
+			justify-content: space-between;
+			align-items: center;
 
 		&__image {
 			border-radius: 50%;
@@ -48,6 +50,41 @@ export default {
 		&__link {
 			font-size: 12px;
 		}
+
+		&__btn {
+			display: block;
+			cursor: pointer;
+			margin: 5px auto;
+			padding: 5px;
+			border-radius: 5px;
+			background-color: transparent;
+			border-color: #002fff;
+			color: #002fff;
+			transition: all .3s ease-in-out;
+			font-size: 15px;
+
+		}
+
+		&__btn:hover {
+			color: #fff;
+			background-color: #002fff;
+		}
+	}
+}
+
+@media screen and (min-width: 768px) {
+	.results {
+		flex-direction: row;
+
+		.result {
+			flex-basis: 45%;
+		}
+	}
+}
+
+@media screen and (min-width: 1024px) {
+	.results .result {
+		flex-basis: 33%;
 	}
 }
 </style>
